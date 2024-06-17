70bdbd06105237620134828bfef1de4099589141
micfro <michalfrost71@gmail.com>
2024-06-17 18:58:56 +0200

    image for post new

--- src/main/java/com/dci/a3m/service/PostServiceImpl.java
+++ src/main/java/com/dci/a3m/service/PostServiceImpl.java
@@ -94,8 +94,8 @@ public class PostServiceImpl implements PostService {
                 "https://images.unsplash.com/photo-1529697216570-f48ef8f6b2dd?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                 "https://images.unsplash.com/photo-1675967682194-2076e2b772e7?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                 "https://images.unsplash.com/photo-1428908728789-d2de25dbd4e2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                "https://unsplash.com/photos/landscape-photo-of-mountains-under-starry-sky-at-nighttime-SHcHVFhz7-M",
                 "https://images.unsplash.com/photo-1502512571217-6a08d302fe5a?q=80&w=1588&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                "https://images.unsplash.com/photo-1505322022379-7c3353ee6291?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                 "https://images.unsplash.com/photo-1465080357990-d4bc259ec4a9?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         };
 
